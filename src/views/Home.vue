1ce7c7dccbc22923e06f56abc9fbcd41b9b83f10
mt-coff <mt.coff.00@gmail.com>
2019-04-03 15:04:23 +0900

    add inital project file

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="CA 平成最後のハッカソン" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
