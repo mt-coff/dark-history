519e907d9e59fdabc4634cfd27d58d102e36092a
ShujiOba <shuji365630@gmail.com>
2019-04-24 23:42:43 +0900

    :sparkles: 最低限のAPIを実装

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,48 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as model from "./model";
+import * as express from "express";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.post("/item", async (req, res) => {
+  const post = model.newPost(req.body);
+  try {
+    const id = await model.postData(post);
+    res.status(200).json({ message: id });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Failed" });
+  }
+});
+app.get("/item/:id", async (req, res) => {
+  try {
+    const post = await model.getPost(req.params.id);
+    res.status(200).json({
+      ...post,
+      createAt:
+        post.createAt instanceof Date
+          ? post.createAt.getMilliseconds()
+          : post.createAt.toMillis()
+    });
+  } catch (e) {
+    res.status(404).json({ message: e });
+  }
+});
+app.get("/items", async (req, res) => {
+  try {
+    const posts = await model.getAllPost();
+    res.status(200).json(
+      posts.map(x => ({
+        ...x,
+        createAt:
+          x.createAt instanceof Date
+            ? x.createAt.getMilliseconds()
+            : x.createAt.toMillis()
+      }))
+    );
+  } catch (e) {
+    res.status(404).json({ message: e });
+  }
+});
+
+export const api = functions.https.onRequest(app);
