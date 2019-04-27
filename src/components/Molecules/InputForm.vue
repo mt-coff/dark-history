d4196bec7ec72e0d25f3420fff15a6d7bf02e96c
ShujiOba <shuji365630@gmail.com>
2019-04-27 22:26:45 +0900

    :art: Add form parts

---
+++ src/components/Molecules/InputForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <form-label>{{ label }}</form-label>
+    <basic-input
+      class="form"
+      @input="e => $emit('input', e)"
+      :value="value"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "input-form",
+  components: {
+    BasicInput: () => import("@/components/Atoms/BasicInput"),
+    FormLabel: () => import("@/components/Atoms/FormLabel")
+  },
+  props: {
+    value: String,
+    placeholder: String,
+    label: String
+  }
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/Molecules/TextareaForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <form-label>{{ label }}</form-label>
+    <basic-textarea @input="e => $emit('input', e)" :value="value" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "textarea-form",
+  components: {
+    FormLabel: () => import("@/components/Atoms/FormLabel"),
+    BasicTextarea: () => import("@/components/Atoms/BasicTextarea")
+  },
+  props: {
+    label: String,
+    value: String
+  }
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+}
+</style>
