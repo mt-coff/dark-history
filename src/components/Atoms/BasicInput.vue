330db89efd8bd27b00bd146914e0478f05bfe0e0
ShujiOba <shuji365630@gmail.com>
2019-04-27 15:30:00 +0900

    :art: Add BasicInput

---
+++ src/components/Atoms/BasicInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    :type="type"
+    @input="e => $emit('input', e.target.value)"
+    :value="value"
+  />
+</template>
+
+<script>
+export default {
+  name: "basic-input",
+  props: {
+    type: {
+      type: String,
+      default: "text"
+    },
+    value: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  border: none;
+  padding: 8px;
+  box-shadow: 0 0 4px 4px rgba(0, 0, 0, 0.5);
+  font-size: 16px;
+}
+</style>
