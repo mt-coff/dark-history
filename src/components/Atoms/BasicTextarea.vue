0d137d3478321a17d4c9f94d4680975bae8fe707
ShujiOba <shuji365630@gmail.com>
2019-04-27 22:25:48 +0900

    :art: Add BasicTextarea

---
+++ src/components/Atoms/BasicTextarea.vue
@@ -0,0 +1,20 @@
+<template>
+  <textarea @input="e => $emit('input', e.target.value)" :value="value" />
+</template>
+
+<script>
+export default {
+  name: "basic-textarea",
+  props: {
+    value: String
+  }
+};
+</script>
+
+<style scoped>
+textarea {
+  border: none;
+  font-size: 16px;
+  padding: 8px;
+}
+</style>
