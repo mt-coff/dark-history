56659a8481dcde83d0886fd8e0c54ad6d007c289
ShujiOba <shuji365630@gmail.com>
2019-04-27 16:37:18 +0900

    :art: Add LogoHeader

---
+++ src/components/Molecules/LogoHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <top>
+    <logo class="logo" />
+  </top>
+</template>
+
+<script>
+import Top from "@/components/Atoms/Top";
+import Logo from "@/components/Atoms/Logo";
+
+export default {
+  name: "logo-header",
+  components: { Top, Logo }
+};
+</script>
+
+<style scoped>
+.logo {
+  width: auto;
+  height: 100%;
+}
+</style>
