3c8e41410c1a40468c232c4c6b86b307638959ee
ShujiOba <shuji365630@gmail.com>
2019-04-27 15:30:29 +0900

    :art: Add FormLabel

---
+++ src/components/Atoms/FormLabel.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+  name: "form-label"
+};
+</script>
+
+<style scoped>
+span {
+  color: var(--light);
+}
+</style>
