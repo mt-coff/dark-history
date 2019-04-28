3a6292d5733505367a5fbbe4478a5e0a3d85c6ea
mt-coff <mt.coff.00@gmail.com>
2019-04-29 02:03:13 +0900

    add ListTemp template

---
+++ src/components/Templates/ListTemp.vue
@@ -0,0 +1,13 @@
+<template>
+  <container></container>
+</template>
+
+<script>
+export default {
+  components: {
+    Container: () => import("@/components/Atoms/Container")
+  }
+};
+</script>
+
+<style scoped></style>
