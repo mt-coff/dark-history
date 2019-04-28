ade51189c353b07fccc01e5832fbcc4c21a2efd8
mt-coff <mt.coff.00@gmail.com>
2019-04-29 04:49:56 +0900

    use v-for

--- src/components/Templates/ListTemp.vue
+++ src/components/Templates/ListTemp.vue
@@ -1,13 +1,41 @@
 <template>
-  <container></container>
+  <container class="d-list" :space="48">
+    <named-coffin
+      v-for="(post, index) in posts"
+      :key="index"
+      class="coffin"
+      :name="post.title"
+    />
+  </container>
 </template>
 
 <script>
 export default {
   components: {
-    Container: () => import("@/components/Atoms/Container")
+    container: () => import("@/components/Atoms/Container"),
+    NamedCoffin: () => import("@/components/Molecules/NamedCoffin")
+  },
+  data() {
+    return {};
+  },
+  props: {
+    posts: {
+      type: Array
+    }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.d-list {
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  justify-content: flex-start;
+}
+.coffin {
+  min-width: 0;
+  width: 300px;
+  margin: 1rem;
+}
+</style>
