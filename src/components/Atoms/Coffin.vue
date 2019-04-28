6286ba0e933ed5a67fce5b625aa1d232e6a52b59
mt-coff <mt.coff.00@gmail.com>
2019-04-29 03:02:40 +0900

    add coffin component

---
+++ src/components/Atoms/Coffin.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="coffin">
+    <div class="coffin-top"></div>
+    <div class="coffin-bottom"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.coffin {
+  position: relative;
+  display: inline-block;
+}
+.coffin::before {
+  content: "";
+  background-color: white;
+  height: 4px;
+  width: 70%;
+  display: inline-block;
+  position: absolute;
+  top: 30%;
+  left: 15%;
+}
+.coffin::after {
+  content: "";
+  background-color: white;
+  height: 60px;
+  width: 5px;
+  display: inline-block;
+  position: absolute;
+  top: 15%;
+  left: 47%;
+}
+.coffin-top {
+  height: 0px;
+  width: 40px;
+  border-bottom: 20px solid black;
+  border-left: 10px solid transparent;
+  border-right: 10px solid transparent;
+}
+.coffin-bottom {
+  height: 0px;
+  width: 40px;
+  border-top: 90px solid black;
+  border-left: 10px solid transparent;
+  border-right: 10px solid transparent;
+}
+</style>
