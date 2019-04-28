5d7ff117eb1a4970bd006b201743b03e78e83d50
mt-coff <mt.coff.00@gmail.com>
2019-04-29 04:50:10 +0900

    add dummies

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <list-temp />
+    <list-temp :posts="dummies" />
   </div>
 </template>
 
@@ -9,6 +9,53 @@ export default {
   name: "list",
   components: {
     ListTemp: () => import("@/components/Templates/ListTemp")
+  },
+  data() {
+    return {
+      // TODO: APIと接続して置き換える
+      dummies: [
+        {
+          id: "hogehoge",
+          title: "ドナルド・トランプとデート",
+          detail: "詳細"
+        },
+        {
+          id: "fugafuga",
+          title: "ドナルド・トランプとデート",
+          detail: "詳細"
+        },
+        {
+          id: "piyopiyo",
+          title: "ドナルド・トランプとデート",
+          detail: "詳細"
+        },
+        {
+          id: "1234",
+          title: "ドナルド・トランプとデート",
+          detail: "詳細"
+        },
+        {
+          id: "456",
+          title: "ドナルド・トランプとデート",
+          detail: "詳細"
+        },
+        {
+          id: "iuorq",
+          title: "ドナルド・トランプとデート",
+          detail: "詳細"
+        },
+        {
+          id: "45678",
+          title: "ドナルド・トランプとデート",
+          detail: "詳細"
+        },
+        {
+          id: "83924",
+          title: "ドナルド・トランプとデート",
+          detail: "詳細"
+        }
+      ]
+    };
   }
 };
 </script>
