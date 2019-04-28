37b2bad1a9d2e26235d9d2a6dbe038e6f8878582
schktjm <si2.o.mjt@gmail.com>
2019-04-29 01:31:17 +0900

    :+1: feature detail page

---
+++ src/components/Molecules/TweetButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="twitter">
+    <img src="@/assets/Twitter_Logo_WhiteOnBlue.svg" class="twitter-logo" />
+    <a
+      href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+      class="twitter-share-button"
+      data-show-count="false"
+      >Tweet</a
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.twitter {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 96px;
+  padding: 0 5px;
+  background-color: var(--twitter);
+  font-family: sans-serif;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  border-radius: 3px;
+}
+
+.twitter-logo {
+  width: 32px;
+}
+
+.twitter-share-button {
+  text-decoration: none;
+  color: white;
+  size: 16px;
+}
+</style>
---
+++ src/components/Templates/DetailTemp.vue
@@ -0,0 +1,33 @@
+<template>
+  <container class="detail-page">
+    <basic-image
+      :url="url"
+      style="width: 800px; height: 540px; margin-bottom: 32px;"
+    ></basic-image>
+    <tweet-button></tweet-button>
+  </container>
+</template>
+
+<script>
+export default {
+  components: {
+    BasicImage: () => import("@/components/Atoms/BasicImage"),
+    Container: () => import("@/components/Atoms/Container"),
+    TweetButton: () => import("@/components/Molecules/TweetButton")
+  },
+  props: {
+    url: {
+      type: String,
+      default: "https://bulma.io/images/placeholders/256x256.png"
+    }
+  }
+};
+</script>
+
+<style scoped>
+.detail-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
