52e28de2f95790361fd1f218de0b734771d5fa23
schktjm <si2.o.mjt@gmail.com>
2019-04-29 21:07:25 +0900

    feature/add-detail-image

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,17 +1,35 @@
 <template>
   <div class="detail">
-    <detail-temp></detail-temp>
+    <detail-temp :url="image.imageURL"></detail-temp>
   </div>
 </template>
 
 <script>
+import { getItem } from "../js/api";
+
 export default {
   name: "detail",
   components: {
     DetailTemp: () => import("@/components/Templates/DetailTemp")
   },
-  methods: {
-    getItem() {}
+  data() {
+    return {
+      image: {
+        id: "",
+        darkness: 0,
+        title: "",
+        detail: "",
+        createAt: 0,
+        imageURL: "",
+        strength: 0
+      }
+    };
+  },
+  created() {
+    getItem(this.$route.params.id)
+      .then(response => response.json())
+      .then(json => (this.image = json))
+      .catch(error => console.error(error));
   }
 };
 </script>
